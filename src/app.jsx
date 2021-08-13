7404d665b8385c8769381facc95afa5f5195ddb6
Elina <elinapark0818@gmail.com>
2021-08-13 18:38:56 +0900

    reset

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,25 @@
-import { Route } from "react-router-dom";
-
-import styles from './app.module.css';
-import About from './components/about/about';
-import Aside from './components/aside/aside';
-import Contact from './components/contact/contact';
-import Navbar from './components/navbar/navbar';
-import Profile from './components/profile/profile';
-import Projects from './components/projects/projects';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className={styles.container}>
-      <header className={styles.menu}>
-        <Navbar>
-          <Route path="/about" exact={true} component={About} />
-          <Route path="/profile" exact={true} component={Profile} />
-          <Route path="/projects" exact={true} component={Projects} />
-          <Route path="/contact" exact={true} component={Contact} />
-        </Navbar>
-        <Aside />
-      </header>
-      <main className={styles.main}>
-      <Profile />
-      <About />
-      <Projects />
-      <Contact />
-      </main>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/profile">Profile</Link>
+      </nav>
+
+      <Switch>
+        <Route path={['/', '/home']} exact>
+          <Home />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
