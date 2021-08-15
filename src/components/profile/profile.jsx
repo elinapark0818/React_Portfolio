77a7c425afa49cef5d9ee0c842471fa7f5663270
Elina <elinapark0818@gmail.com>
2021-08-16 01:46:58 +0900

    font, css

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import styles from './profile.module.css';
 
-const Profile = (props) => <h1>profile</h1>
+const Profile = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <img className={styles.avatar} 
+            src="/img/profile_img/elina.jpg" 
+            alt="profile_avatar" 
+          />
+        </div>
+      <div className={styles.title}>
+        <h3>이름 : 박 윤 정</h3>
+        <h3>생년월일 : 1992.06.04</h3>
+        <h3>지원분야 : Front-end developer</h3>
+        <h3>PS : </h3>
+        </div>
+    </div>
+  )
+}
 
 export default Profile;
