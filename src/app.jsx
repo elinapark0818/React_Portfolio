1ea94cc8fdf7c9e78cc1945a69fb66ec4763f1bc
Elina <elinapark0818@gmail.com>
2021-08-18 22:41:14 +0900

    갈아엎기!

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,21 @@
 import styles from './app.module.css';
 import Home from './components/home/home';
-import Profile from './components/profile/profile';
-import Projects from './components/projects/projects';
-import Contact from './components/contact/contact';
-import Menu from './components/menu/menu';
-import Skills from './components/skills/skills';
+// import Profile from './components/profile/profile';
+// import Projects from './components/projects/projects';
+// import Contact from './components/contact/contact';
+// import Menu from './components/menu/menu';
+// import Skills from './components/skills/skills';
+import { useSpring, animated } from 'react-spring'
 
 function App() {
+  const display = useSpring({ 
+    to: { opacity: 1, marginTop: 0}, from: { opacity: 0, marginTop: -500 } })
+
   return (
     <div className={styles.app}>
-      <Menu />
-      <Home />
-      <Profile />
-      <Skills />
-      <Projects />
-      <Contact />
+      <animated.div style={display}>
+        <Home />
+      </animated.div>
     </div>
   );
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './home.module.css';
 
-const Home = (props) => {
+const Home = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Front-end 개발자  박윤정의 포트폴리오입니다.</h1>
