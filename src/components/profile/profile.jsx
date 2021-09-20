8d250a088650748f1503cce1e9deb11a2437c635
Elina <elinapark0818@gmail.com>
2021-09-20 21:10:03 +0900

    profile css

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -23,16 +23,14 @@ const Profile = () => {
           <button className={styles.profile_btn}>Profile</button>
           </div>
 
-          <animated.div style={toTop}>
+          <main className={styles.main}>
             <div className={styles.image}>
               <img className={styles.avatar} 
                 src="/img/profile_img/profile_img.jpg" 
                 alt="profile_img" 
               />
               </div>
-          </animated.div>
 
-          <animated.div style={toTop2}>
             <div className={styles.description}>
               <h3>이름 : 박 윤 정<br/>
               생년월일 : 1992.06.04<br/>
@@ -41,13 +39,14 @@ const Profile = () => {
               스마일게이트RPG GM (12개월)<br/>
               </h3>
             <br/>
-              <h3>"사용성을 고려하여 편안한 기능을 구현하는 <br/>
-              매력적인 개발자가 될겁니다. <br/>
-              도전을 두려워하지 않고, 새로운 것에 열광하며 <br/>
+              <h3>"사용성을 고려하여 설계하고<br/>
+              편안한 기능을 구현하는 개발자가 될겁니다. <br/>
+              도전을 두려워하지 않고, 기본에 충실하여 <br/>
               나만의 분야를 개척하여 인정받는 <br/> 
               멋있는 인생을 사는 것이 최종 목표입니다."</h3>
             </div>
-          </animated.div>
+
+          </main>
           </div>
       </div>
       
