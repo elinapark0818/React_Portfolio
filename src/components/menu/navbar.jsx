332f1acd778ba643dddfb7da7e1c00940b0254e6
Elina <elinapark0818@gmail.com>
2021-09-21 01:50:42 +0900

    add navbar scrollintoview

--- src/components/menu/navbar.jsx
+++ src/components/menu/navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import './navbar.css';
 import { FcMenu } from "react-icons/fc";
+import { useRef } from 'react/cjs/react.development';
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(true)
@@ -24,18 +25,57 @@ export default function Navbar() {
 
   }, [])
 
+  const navRef = useRef([]);
+
+  const [currentNav, setCurrentNav] = useState();
+
+  {nativeTouchData.map((data, index) => (
+    <Nav
+    key={index}
+    onClick={() => {
+      navRef.current[index].scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+      setCurrentNav(navRef.current[index]);
+    }}
+      seleted={navRef.current[index] === currentNav}
+    >
+
+    </Nav>
+  ))}
+
   
   return (
     <div className="container">
     <nav>
       {(toggleMenu || screenWidth > 1000) && (
         <ul className="list">
-          <li className="items">Home</li>
-          <li className="items">Profile</li>
-          <li className="items">About</li>
-          <li className="items">Skills</li>
-          <li className="items">Projects</li>
-          <li className="items">Contact</li>
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[0] = cp))}><div><Home/></div>
+          </button>
+          
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[1] = cp))}><div><Profile/></div>
+          </button>
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[2] = cp))}><div><About/></div>
+          </button>
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[3] = cp))}><div><Skills/></div>
+          </button>
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[4] = cp))}><div><Projects/></div>
+          </button>
+          <button 
+          className="items"
+          ref={(cp => (navRef.current[5] = cp))}><div><Contact/></div>
+          </button>
         </ul>
       )}
 
